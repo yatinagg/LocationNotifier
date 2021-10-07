a4321c86480684097639e3d12b94c3b4713247e7
yatinagg <yatinagg12@gmail.com>
2021-10-07 19:13:01 +0530

    Updated

--- app/src/main/java/com/example/locationnotifier/GeofenceHelper.java
+++ app/src/main/java/com/example/locationnotifier/GeofenceHelper.java
@@ -1,5 +1,6 @@
 package com.example.locationnotifier;
 
+import android.annotation.SuppressLint;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.ContextWrapper;
@@ -19,16 +20,16 @@ public class GeofenceHelper extends ContextWrapper {
         super(base);
     }
 
-    public GeofencingRequest geofencingRequest(Geofence geofence){
+    public GeofencingRequest geofencingRequest(Geofence geofence) {
         return new GeofencingRequest.Builder()
                 .addGeofence(geofence)
                 .setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
                 .build();
     }
 
-    public Geofence getGeofence(String ID, LatLng latLng, float radius, int transitionTypes){
+    public Geofence getGeofence(String ID, LatLng latLng, float radius, int transitionTypes) {
         return new Geofence.Builder()
-                .setCircularRegion(latLng.latitude,latLng.longitude,radius)
+                .setCircularRegion(latLng.latitude, latLng.longitude, radius)
                 .setRequestId(ID)
                 .setTransitionTypes(transitionTypes)
                 .setLoiteringDelay(5000)
@@ -36,25 +37,26 @@ public class GeofenceHelper extends ContextWrapper {
                 .build();
     }
 
-    public PendingIntent getPendingIntent(){
-        if(pendingIntent != null)
+    @SuppressLint("UnspecifiedImmutableFlag")
+    public PendingIntent getPendingIntent() {
+        if (pendingIntent != null)
             return pendingIntent;
-        Intent intent = new Intent(this,GeofenceBroadcastReceiver.class);
-        pendingIntent = PendingIntent.getBroadcast(this,1410,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        Intent intent = new Intent(this, GeofenceBroadcastReceiver.class);
+        pendingIntent = PendingIntent.getBroadcast(this, 1410, intent, PendingIntent.FLAG_UPDATE_CURRENT);
         return pendingIntent;
     }
 
-    public String getErrorString(Exception e){
-        if(e instanceof ApiException){
+    public String getErrorString(Exception e) {
+        if (e instanceof ApiException) {
             ApiException apiException = (ApiException) e;
-            switch (apiException.getStatusCode()){
+            switch (apiException.getStatusCode()) {
                 case GeofenceStatusCodes
                         .GEOFENCE_NOT_AVAILABLE:
                     return getString(R.string.geofence_not_available);
                 case GeofenceStatusCodes
                         .GEOFENCE_TOO_MANY_GEOFENCES:
                     return getString(R.string.too_many_geofences);
-                case  GeofenceStatusCodes
+                case GeofenceStatusCodes
                         .GEOFENCE_TOO_MANY_PENDING_INTENTS:
                     return getString(R.string.too_many_pending_intents);
             }
--- app/src/main/java/com/example/locationnotifier/MapsActivity.java
+++ app/src/main/java/com/example/locationnotifier/MapsActivity.java
@@ -1,26 +1,26 @@
 package com.example.locationnotifier;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.widget.ThemedSpinnerAdapter;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.fragment.app.FragmentActivity;
-
-import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
-import android.location.Address;
 import android.location.Geocoder;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofencingClient;
@@ -30,48 +30,50 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.example.locationnotifier.databinding.ActivityMapsBinding;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 
-import java.io.IOException;
-import java.util.List;
 import java.util.Locale;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private static final String TAG = "MapsActivity";
+    private final int REQUEST_CODE_PERMISSIONS = 1410;
+    private final String[] REQUIRED_PERMISSIONS = new String[]{"android.permission.ACCESS_FINE_LOCATION"};
     private GoogleMap mMap;
     private GeofencingClient geofencingClient;
-    private final int FINE_LOCATION_ACCESS_REQUEST_CODE = 10001;
-    private float radius = 200;
-    private String city;
-    private String pin;
+    private Geocoder geocoder;
+    private float radius = 500;
     private GeofenceHelper geofenceHelper;
     private double lat = 28.6482929;
     private double lng = 77.3720005;
 
-    Button button;
-    EditText editTextLatLong;
-    EditText editTextRadius;
-    TextView textView;
-
-
+    private Button button;
+    private EditText editTextLatLong;
+    private EditText editTextRadius;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_maps);
 
+
+        if (allPermissionsGranted())
+            startMaps();
+        else
+            ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+
+    }
+
+    private void startMaps() {
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
+        if (mapFragment == null)
+            return;
         mapFragment.getMapAsync(this);
 
         geofencingClient = LocationServices.getGeofencingClient(this);
@@ -81,50 +83,41 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         editTextLatLong = (EditText) findViewById(R.id.editTextLatLong);
         editTextRadius = (EditText) findViewById(R.id.editTextRadius);
         textView = (TextView) findViewById(R.id.textViewLocality);
-        Geocoder geocoder = new Geocoder(this, Locale.getDefault());
+        geocoder = new Geocoder(this, Locale.getDefault());
+        setLocation();
+
+        setupListener();
+    }
+
+    private void setLocation(){
         try {
-            city = geocoder.getFromLocation(lat,lng,1).get(0).getLocality();
-            String pin = geocoder.getFromLocation(lat,lng,1).get(0).getPostalCode();
-            Log.d(TAG,city);
-            Log.d(TAG,pin);
-            if(city == null)
+            String addressLine = geocoder.getFromLocation(lat, lng, 1).get(0).getAddressLine(0);
+            String city = geocoder.getFromLocation(lat, lng, 1).get(0).getLocality();
+            String pin = geocoder.getFromLocation(lat, lng, 1).get(0).getPostalCode();
+            if (city.equals("null"))
                 textView.setText(city);
+            else if(addressLine.equals("null"))
+                textView.setText(addressLine);
             else
                 textView.setText(pin);
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if(!validateTextFields()){
-                    return;
-                }
-                String latLong = editTextLatLong.getText().toString();
-                String[] latLongArray = latLong.split(",");
-                lat = Double.parseDouble(latLongArray[0]);
-                lng = Double.parseDouble(latLongArray[1]);
-                radius = Float.parseFloat(editTextRadius.getText().toString());
-                LatLng latLng = new LatLng(lat,lng);
+    }
 
-                mMap.clear();
-                addMarker(latLng);
-                addCircle(latLng,radius);
-                addGeofence(latLng,radius);
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,15));
-                try {
-                    city = geocoder.getFromLocation(lat,lng,1).get(0).getLocality();
-                    pin = geocoder.getFromLocation(lat,lng,1).get(0).getPostalCode();
-                    if(city.equals(getString(R.string.nullString)))
-                        textView.setText(city);
-                    else {
-                        textView.setText(pin);
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
+    private void setupListener() {
+        button.setOnClickListener(view -> {
+            if (!validateTextFields())
+                return;
+            LatLng latLng = new LatLng(lat, lng);
+
+            mMap.clear();
+            addMarker(latLng);
+            addCircle(latLng, radius);
+            addGeofence(latLng, radius);
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15));
+            setLocation();
         });
     }
 
@@ -138,89 +131,106 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             editTextRadius.setError(getString(R.string.required_field));
             valid = false;
         }
+        if (valid) {
+            String latLong = editTextLatLong.getText().toString();
+            if (!latLong.contains(",")) {
+                editTextLatLong.setError(getString(R.string.invalid));
+                return false;
+            }
+            String[] latLongArray = latLong.split(",");
+            if (latLongArray.length != 2) {
+                editTextLatLong.setError(getString(R.string.invalid));
+                return false;
+            }
+            lat = Double.parseDouble(latLongArray[0]);
+            lng = Double.parseDouble(latLongArray[1]);
+            if (lat < -90 || lat > 90 || lng <= -180 || lng > 180) {
+                editTextLatLong.setError(getString(R.string.invalid));
+                return false;
+            }
+            radius = Float.parseFloat(editTextRadius.getText().toString());
+        }
         return valid;
     }
 
-            @Override
-    public void onMapReady(GoogleMap googleMap) {
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) {
         mMap = googleMap;
 
         // Add a marker in Delhi and move the camera
-        LatLng delhi = new LatLng(28.6482929,77.3720005);
-        mMap.addMarker(new MarkerOptions().position(delhi).title(getString(R.string.marker_in_delhi)));
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(delhi,15));
-        enableUserLocation();
-        addGeofence(delhi,radius);
-        addCircle(delhi,radius);
+        LatLng delhi = new LatLng(28.6482929, 77.3720005);
+        mMap.addMarker(new MarkerOptions().position(delhi).title(getString(R.string.marker_in_delhi)).icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(delhi, 15));
+        addGeofence(delhi, radius);
+        addCircle(delhi, radius);
     }
 
-    private void enableUserLocation(){
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
-            mMap.setMyLocationEnabled(true);
-        }
-        else{
-            // Ask for permission
-            if(ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.ACCESS_FINE_LOCATION)){
-                // We need to show user a dialog for displaying why the permission is needed and then ask for the permission
-                ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_ACCESS_REQUEST_CODE);
-            }
-            else{
-                ActivityCompat.requestPermissions(this,new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, FINE_LOCATION_ACCESS_REQUEST_CODE);
-            }
-        }
-    }
 
     @SuppressLint("MissingPermission")
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if(requestCode == FINE_LOCATION_ACCESS_REQUEST_CODE){
-            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                // we have the permission
-                mMap.setMyLocationEnabled(true);
-            }
-            else{
-                // we do not have the permission
-                Log.d(TAG,getString(R.string.no_permission));
-            }
-        }
-    }
-
-    @SuppressLint("MissingPermission")
-    private void addGeofence(LatLng latLng, float radius){
+    private void addGeofence(LatLng latLng, float radius) {
 
         String GEOFENCE_ID = getString(R.string.geo_fence_id);
-        Geofence geofence = geofenceHelper.getGeofence(GEOFENCE_ID,latLng,radius,Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_DWELL | Geofence.GEOFENCE_TRANSITION_EXIT);
+        Geofence geofence = geofenceHelper.getGeofence(GEOFENCE_ID, latLng, radius, Geofence.GEOFENCE_TRANSITION_ENTER | Geofence.GEOFENCE_TRANSITION_DWELL | Geofence.GEOFENCE_TRANSITION_EXIT);
         GeofencingRequest geofencingRequest = geofenceHelper.geofencingRequest(geofence);
         PendingIntent pendingIntent = geofenceHelper.getPendingIntent();
 
         geofencingClient.addGeofences(geofencingRequest, pendingIntent)
-                .addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void unused) {
-                        Log.d(TAG,getString(R.string.onSuccess));
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        String errorMessage = geofenceHelper.getErrorString(e);
-                        Log.d(TAG,"onFailure: " + errorMessage);
-                    }
+                .addOnSuccessListener(unused -> Log.d(TAG, getString(R.string.onSuccess)))
+                .addOnFailureListener(e -> {
+                    String errorMessage = geofenceHelper.getErrorString(e);
+                    Log.d(TAG, "onFailure: " + errorMessage);
                 });
     }
 
-    private void addMarker(LatLng latLng){
-        mMap.addMarker(new MarkerOptions().position(latLng));
+    private void addMarker(LatLng latLng) {
+        mMap.addMarker(new MarkerOptions().position(latLng).icon(BitmapDescriptorFactory.fromResource(R.drawable.marker)));
     }
 
-    private void addCircle(LatLng latLng,float radius){
+    private void addCircle(LatLng latLng, float radius) {
         CircleOptions circleOptions = new CircleOptions();
         circleOptions.center(latLng);
         circleOptions.radius(radius);
-        circleOptions.strokeColor(Color.argb(255,231,242,118));
-        circleOptions.fillColor(Color.argb(200,227,182,183));
+        circleOptions.strokeColor(Color.argb(255, 231, 242, 118));
+        circleOptions.fillColor(Color.argb(200, 227, 182, 183));
         circleOptions.strokeWidth(4);
         mMap.addCircle(circleOptions);
+    }
+
+    private boolean allPermissionsGranted() {
+        for (String permission : REQUIRED_PERMISSIONS)
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                return false;
+            }
+        return true;
+    }
+
+    public void createNotification(String title, String text){
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel = new NotificationChannel("My Notification","Notification Name",NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.cancelAll();
+            manager.createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(MapsActivity.this,"My Notification");
+        builder.setContentTitle(title);
+        builder.setContentText(text);
+        builder.setSmallIcon(R.drawable.marker);
+        builder.setAutoCancel(true);
+
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(MapsActivity.this);
+        managerCompat.notify(14,builder.build());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted())
+                startMaps();
+            else {
+                ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
+            }
+        }
     }
 }
